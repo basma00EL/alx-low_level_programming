6437c81238c56584d2fb4929e788af10dc30ae15
basma00EL <elhajlibasma@gmail.com>
2023-06-23 10:18:46 -0700

    _isuper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+
 int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
@@ -8,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int mul(int a, int b);
+
+#endif /* MAIN_H */
